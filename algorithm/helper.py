ad005946ef36f7c27b92167f9793ecc2c49f7b5f
Budi Pang <budi.pangestu.t@gmail.com>
2018-05-08 20:28:25 +0700

    add debugger line

--- algorithm/helper.py
+++ algorithm/helper.py
@@ -1,4 +1,7 @@
+import code
+
 def dominate(obj1, obj2, dimensi):
+    code.interact(local=dict(globals(), **locals()))
     dominate_status = 0
     for i in range(0, dimensi):
         if obj1[i] > obj2[i]:
